94b3ad4b7e97254d995a4af8f2a2feb5261dcd04
Robert Hernandez <broberto102@gmail.com>
2020-11-07 21:10:07 -0600

    fixes to the java

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-desciption p");
+const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 
